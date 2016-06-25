ac48f14e599f962f24a9712cd3f6bc1eabe6b81b
PeterTong <tkwpeter21@gmail.com>
2016-06-26 00:10:46 +0800

    Completed Part 4

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by KwokWing Tong on 25/6/2016.
+//  Copyright © 2016 Tong Kwok Wing. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
