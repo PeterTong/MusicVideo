f4972d69d031110271c895183516f79af03d78c0
PeterTong <tkwpeter21@gmail.com>
2016-06-28 10:55:09 +0800

    Completed Part 9

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -12,16 +12,43 @@ class ViewController: UIViewController {
   
   var videos = [Videos]()
 
+  @IBOutlet weak var displatLabel: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    
     
     //Call API
     let api = APIManager()
     
     api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
     
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(reachabilityStatusChanged), name: "ReachStatusChanged", object: nil)
+    
+    // When "ReachStatusChanged" in Push Notification method is changed , then will call the function reachabilityStatusChanged
+    
+    reachabilityStatusChanged()
   }
 
+  func reachabilityStatusChanged(){
+    switch reachabilityStatus {
+    case NOACCESS : view.backgroundColor = UIColor.redColor()
+    displatLabel.text = "No Internet"
+    case WIFI : view.backgroundColor = UIColor.greenColor()
+    displatLabel.text = "Reachable with WIFI"
+    case WWAN : view.backgroundColor = UIColor.yellowColor()
+    displatLabel.text = "Reachable with Cellular"
+    default:return
+    }
+  }
+  
+  // Is called just as the object is about to be deallocated
+  deinit
+  {
+    NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+  }
+  
+  
   func didLoadData(videos: [Videos]){
     
     print(reachabilityStatus)
