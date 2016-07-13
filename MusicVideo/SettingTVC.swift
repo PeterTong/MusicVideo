92e10c307413057f93dfd3df215c1434918867e5
PeterTong <tkwpeter21@gmail.com>
2016-07-13 14:05:42 +0800

    Completed Part-22 connect IBOutlet to controller, make a preferred Font

---
+++ MusicVideo/SettingTVC.swift
@@ -0,0 +1,64 @@
+//
+//  SettingTVC.swift
+//  MusicVideo
+//
+//  Created by KwokWing Tong on 13/7/2016.
+//  Copyright © 2016 Tong Kwok Wing. All rights reserved.
+//
+
+import UIKit
+
+class SettingTVC: UITableViewController {
+	
+	@IBOutlet weak var aboutDisplay: UILabel!
+	
+	
+	@IBOutlet weak var feedbackDisplay: UILabel!
+	
+	@IBOutlet weak var securityDisplay: UILabel!
+	
+	@IBOutlet weak var bestImageDisplay: UILabel!
+	
+	@IBOutlet weak var touchID: UISwitch!
+	
+	@IBOutlet weak var APICnt: UILabel!
+	
+	@IBOutlet weak var sliderCnt: UISlider!
+	
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(preferredFontChange), name: UIContentSizeCategoryDidChangeNotification, object: nil)// This is a way to viewcontroller for observer font size changed
+		
+		tableView.alwaysBounceVertical = false
+		
+		
+		
+		
+	}
+	
+	func preferredFontChange(){
+		
+		print("The preferred font is changed")
+		
+		aboutDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+		feedbackDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+		securityDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+		bestImageDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+		APICnt.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+	}
+	
+	deinit
+	{
+		
+		
+		NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification,object: nil)
+	}
+	
+	
+	
+	
+	
+}
